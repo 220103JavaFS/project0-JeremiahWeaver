88abf99933cd78ae84c900500cc85b64482ac574
Jeremiah Weaver <jeremiah.weaver@revature.net>
2022-01-19 17:07:32 -0800

    Some Subtraction

---
+++ ProjectZeroVersion2/PZFC/src/main/java/com/pzfc/utls/RookieFinderNeeds.java
@@ -0,0 +1,18 @@
+package com.pzfc.utls;
+
+public class RookieFinderNeeds {
+
+    public static int Missing(){
+
+      int Rookie = 10;
+
+      int  RookieFinder = 3;
+
+      int missing = Rookie - RookieFinder;
+
+      return missing;
+
+    }
+
+
+}
---
+++ ProjectZeroVersion2/PZFC/src/main/java/com/pzfc/utls/SignedNeeds.java
@@ -0,0 +1,16 @@
+package com.pzfc.utls;
+
+public class SignedNeeds {
+
+    public static int SignedNeeds(){
+
+        int cards = 30;
+
+        int signed = 8;
+
+        int signedNeeds = cards - signed;
+
+        return signedNeeds;
+
+    }
+}
